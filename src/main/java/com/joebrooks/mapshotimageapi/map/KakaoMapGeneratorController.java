5be986fdc4c9d271a4c71a19069479f4e68bfe51
Chanwoo Lee <lcw3176@naver.com>
2022-05-05 16:43:45 +0900

    Feature/구글 지도 추가 (#1)
    * 구글 지도 추가
    
    * crossOrigin 어노테이션 추가

--- src/main/java/com/joebrooks/mapshotimageapi/map/KakaoMapGeneratorController.java
+++ src/main/java/com/joebrooks/mapshotimageapi/map/KakaoMapGeneratorController.java
@@ -1,14 +1,16 @@
-package com.joebrooks.mapshotimageapi.map.kakao;
+package com.joebrooks.mapshotimageapi.map;
 
 import com.joebrooks.mapshotimageapi.websocket.UserMapRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/map/gen/kakao")
+@CrossOrigin("https://richshrimp.tk")
 public class KakaoMapGeneratorController {
 
     @GetMapping
