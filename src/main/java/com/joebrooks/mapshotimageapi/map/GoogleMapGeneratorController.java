9c33c15096ca823fec8530d7467f8b54a2d76c88
lcw3176 <lcw3176@naver.com>
2022-05-05 16:41:03 +0900

    구글 지도 추가

---
+++ src/main/java/com/joebrooks/mapshotimageapi/map/GoogleMapGeneratorController.java
@@ -0,0 +1,23 @@
+package com.joebrooks.mapshotimageapi.map;
+
+import com.joebrooks.mapshotimageapi.websocket.UserMapRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/map/gen/google")
+@CrossOrigin("https://richshrimp.tk")
+public class GoogleMapGeneratorController {
+
+    @GetMapping
+    public String getKakaoMap(@ModelAttribute UserMapRequest mapRequest, Model model){
+        model.addAttribute("mapRequest", mapRequest);
+
+        return "map/google";
+    }
+
+}
