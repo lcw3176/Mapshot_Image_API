6acd997cb1d37548453c344fd0bac9e64b51fff5
lcw3176 <lcw3176@naver.com>
2022-06-07 23:34:09 +0900

    패키지 이동

--- src/main/java/com/joebrooks/mapshotimageapi/map/kakao/KakaoMapGeneratorController.java
+++ src/main/java/com/joebrooks/mapshotimageapi/map/kakao/KakaoMapGeneratorController.java
@@ -1,6 +1,6 @@
-package com.joebrooks.mapshotimageapi.map;
+package com.joebrooks.mapshotimageapi.map.kakao;
 
-import com.joebrooks.mapshotimageapi.websocket.UserMapRequest;
+import com.joebrooks.mapshotimageapi.map.UserMapRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
