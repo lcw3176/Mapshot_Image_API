d16fb80ca654ec748ed6039b1e5c4cca53f69509
ChanwooLee <lcw3176@naver.com>
2022-06-07 16:12:17 +0900

    scrollPage 추가

--- src/main/java/com/joebrooks/mapshotimageapi/driver/DriverService.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/DriverService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponents;
 
@@ -14,12 +15,24 @@ public class DriverService{
     private final ChromeDriverExtends chromeDriverExtends;
     private final WebDriverWait webDriverWait;
 
-
-    public byte[] capturePage(UriComponents uri){
+    public void loadPage(UriComponents uri){
         chromeDriverExtends.get(uri.toString());
         webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("checker_true")));
+    }
 
-        return chromeDriverExtends.getFullScreenshot();
+    public void scrollPage(int x, int y){
+        StringBuilder sb = new StringBuilder();
+        sb.append("scroll(");
+        sb.append(x);
+        sb.append(",");
+        sb.append(y);
+        sb.append(")");
+
+        chromeDriverExtends.executeScript(sb.toString());
+    }
+
+    public ByteArrayResource capturePage(){
+        return new ByteArrayResource(chromeDriverExtends.getScreenshot());
     }
 
 }
