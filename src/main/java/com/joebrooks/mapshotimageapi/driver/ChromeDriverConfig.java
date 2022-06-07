ef78f6256918bbff38aef28da7efde1d32a5a261
ChanwooLee <lcw3176@naver.com>
2022-06-07 16:11:42 +0900

    스크롤바 옵션 추가

--- src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/ChromeDriverConfig.java
@@ -1,6 +1,7 @@
 package com.joebrooks.mapshotimageapi.driver;
 
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v99.network.Network;
@@ -30,6 +31,7 @@ public class ChromeDriverConfig {
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
+        options.addArguments("--hide-scrollbars");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--incognito");
 
@@ -39,6 +41,7 @@ public class ChromeDriverConfig {
     @Bean(destroyMethod = "quit")
     public ChromeDriverExtends chromeDriverExtends() throws Exception {
         ChromeDriverExtends chromeDriverExtends = new ChromeDriverExtends(chromeOptions());
+        chromeDriverExtends.manage().window().setSize(new Dimension(1000, 1000));
         DevTools devTools = chromeDriverExtends.getDevTools();
         devTools.createSession();
 
