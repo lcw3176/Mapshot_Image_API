5886e5fb265592f65556f6ab58fdc953a05221f8
Chanwoo Lee <lcw3176@naver.com>
2022-06-26 01:51:50 +0900

    Hotfix/로깅설정 (#11)
    * 로그 설정
    
    * 익셉션 생성자 추가

--- src/main/java/com/joebrooks/mapshotimageapi/driver/exception/LoadPageException.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/exception/LoadPageException.java
@@ -2,6 +2,10 @@ package com.joebrooks.mapshotimageapi.driver.exception;
 
 public class LoadPageException extends RuntimeException {
 
+    public LoadPageException(Throwable e){
+        super(e);
+    }
+
     public LoadPageException(String s, Throwable e){
         super(s, e);
     }
--- src/main/java/com/joebrooks/mapshotimageapi/driver/exception/ScreenshotException.java
+++ src/main/java/com/joebrooks/mapshotimageapi/driver/exception/ScreenshotException.java
@@ -2,6 +2,10 @@ package com.joebrooks.mapshotimageapi.driver.exception;
 
 public class ScreenshotException extends RuntimeException {
 
+    public ScreenshotException(Throwable e) {
+        super(e);
+    }
+
     public ScreenshotException(String s, Throwable e){
         super(s, e);
     }
