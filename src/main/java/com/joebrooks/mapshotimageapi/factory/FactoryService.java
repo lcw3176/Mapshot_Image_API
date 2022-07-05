48a7c47bd0cea7f76f5052a9dde8b0e75d03d0aa
lcw3176 <lcw3176@naver.com>
2022-07-06 01:20:02 +0900

    factory 개편

--- src/main/java/com/joebrooks/mapshotimageapi/factory/FactoryService.java
+++ src/main/java/com/joebrooks/mapshotimageapi/factory/FactoryService.java
@@ -1,31 +1,31 @@
 package com.joebrooks.mapshotimageapi.factory;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joebrooks.mapshotimageapi.driver.DriverService;
 import com.joebrooks.mapshotimageapi.global.model.UserMapRequest;
 import com.joebrooks.mapshotimageapi.global.model.UserMapResponse;
 import com.joebrooks.mapshotimageapi.global.sns.SlackClient;
 import com.joebrooks.mapshotimageapi.global.util.UriGenerator;
 import com.joebrooks.mapshotimageapi.global.util.WidthExtractor;
-import com.joebrooks.mapshotimageapi.storage.StorageManager;
+import com.joebrooks.mapshotimageapi.storage.StorageInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
-import java.io.IOException;
+import javax.annotation.PostConstruct;
+import java.util.Queue;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /*
 알맞은 좌표의 위성 이미지를 요청한 후 이미지를 크롤링합니다.
 한 번에 하나의 이미지 작업만 수행하며, 이미지를 분할해서 캡쳐한 후
 해당 이미지가 전체 이미지 중 어느 위치에서 캡쳐된 이미지인지 나타내주는 x,y 좌표값과
-uuid 값을 유저에게 웹소켓으로 전송해줍니다.
+uuid 값을 제작합니다.
 */
 
 @Service
@@ -36,66 +36,79 @@ public class FactoryService {
     @Value("${map.image.dividedWidth}")
     private int dividedWidth;
 
-
-    private final StorageManager storageManager;
+    private volatile Queue<FactoryTask> userMapRequestsQueue = new ConcurrentLinkedQueue<>();
+    private final ApplicationEventPublisher applicationEventPublisher;
     private final DriverService driverService;
     private final SlackClient slackClient;
-    private final ObjectMapper mapper = new ObjectMapper();
 
+    public void addTask(UserMapRequest request, WebSocketSession session){
+        userMapRequestsQueue.offer(FactoryTask.builder()
+                .userMapRequest(request)
+                .session(session)
+                .build());
+    }
 
-    @Async
-    public void execute(UserMapRequest request, WebSocketSession session) throws IOException {
+    @PostConstruct
+    private void init(){
+        Thread thread = new Thread(() -> {
 
-        if(!session.isOpen()){
-            return;
-        }
+            while(true){
+                while(!userMapRequestsQueue.isEmpty()){
+                    FactoryTask task = userMapRequestsQueue.poll();
+                    try{
+                        driverService.loadPage(UriGenerator.getUri(task.getUserMapRequest()));
+                        int width = WidthExtractor.extract(task.getUserMapRequest());
 
-        try{
-            driverService.loadPage(UriGenerator.getUri(request));
-            int width = WidthExtractor.extract(request);
+                        for(int y = 0; y < width; y+= dividedWidth){
+                            for(int x = 0; x < width; x+= dividedWidth){
 
-            for(int y = 0; y < width; y+= dividedWidth){
-                for(int x = 0; x < width; x+= dividedWidth){
-                    try {
-                        driverService.scrollPage(x, y);
-                        ByteArrayResource byteArrayResource = driverService.capturePage();
+                                driverService.scrollPage(x, y);
+                                ByteArrayResource byteArrayResource = driverService.capturePage();
 
-                        // 세션이 열려있다면 이미지 uuid 전송, 닫혔다면 작업 정지
-                        if(session.isOpen()){
-                            String uuid = UUID.randomUUID().toString();
-                            storageManager.add(uuid, byteArrayResource);
+                                // 세션이 열려있다면 이미지 uuid 전송, 닫혔다면 작업 정지
+                                if(task.getSession().isOpen()){
+                                    String uuid = UUID.randomUUID().toString();
 
-                            UserMapResponse response = UserMapResponse.builder()
-                                    .index(0)
-                                    .x(x)
-                                    .y(y)
-                                    .uuid(uuid)
-                                    .build();
+                                    applicationEventPublisher.publishEvent(StorageInfo.builder()
+                                            .uuid(uuid)
+                                            .byteArrayResource(byteArrayResource)
+                                            .build());
 
-                            session.sendMessage(new TextMessage(mapper.writeValueAsString(response)));
-                        } else {
-                            return;
+                                    applicationEventPublisher.publishEvent(UserMapResponse.builder()
+                                            .index(0)
+                                            .x(x)
+                                            .y(y)
+                                            .uuid(uuid)
+                                            .session(task.getSession())
+                                            .build());
+
+                                } else {
+                                    return;
+                                }
+                            }
                         }
 
+
                     } catch (Exception e){
+                        applicationEventPublisher.publishEvent(UserMapResponse.builder()
+                                .index(0)
+                                .error(true)
+                                .build());
+
                         log.error(e.getMessage(), e);
                         slackClient.sendMessage(e);
                     }
                 }
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
 
+        });
 
-        } catch (Exception e){
-            UserMapResponse response = UserMapResponse.builder()
-                    .index(0)
-                    .error(true)
-                    .build();
-
-            session.sendMessage(new TextMessage(mapper.writeValueAsString(response)));
-            log.error(e.getMessage(), e);
-            slackClient.sendMessage(e);
-        }
-
+        thread.start();
     }
-
 }
