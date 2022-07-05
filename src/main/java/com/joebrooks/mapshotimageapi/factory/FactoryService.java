5efcb6d81713bbffff2f0cf0846a4244dd2d5a7d
lcw3176 <lcw3176@naver.com>
2022-07-06 01:48:21 +0900

    조건 추가

--- src/main/java/com/joebrooks/mapshotimageapi/factory/FactoryService.java
+++ src/main/java/com/joebrooks/mapshotimageapi/factory/FactoryService.java
@@ -80,7 +80,9 @@ public class FactoryService {
                                 }
                             }
 
-          
+                            if(!isRun){
+                                break;
+                            }
                         }
 
 
