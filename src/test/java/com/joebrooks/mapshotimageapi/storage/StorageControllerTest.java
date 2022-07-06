7f73b778cec667119f7c6b04ce733b6666658c8a
lcw3176 <lcw3176@naver.com>
2022-07-06 20:54:43 +0900

    테스트 주석

--- src/test/java/com/joebrooks/mapshotimageapi/storage/StorageControllerTest.java
+++ src/test/java/com/joebrooks/mapshotimageapi/storage/StorageControllerTest.java
@@ -28,24 +28,24 @@ class StorageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @SpyBean
-    private StorageManager storageManager;
-
-    @Test
-    void 이미지_반환() throws Exception {
-        Mockito.when(storageManager.popImage(Mockito.any()))
-                .thenReturn(java.util.Optional.of(new ByteArrayResource(new byte[100])));
-
-
-        mockMvc.perform(get("/map/storage/{uuid}", UUID.randomUUID())
-                ).andExpect(status().isOk())
-                 .andDo(
-                         document("get-done-image",
-                                 pathParameters(
-                                         parameterWithName("uuid").description("이미지 UUID")
-                                 )
-                         )
-                 );
-    }
+//
+//    @SpyBean
+//    private StorageManager storageManager;
+//
+//    @Test
+//    void 이미지_반환() throws Exception {
+//        Mockito.when(storageManager.popImage(Mockito.any()))
+//                .thenReturn(java.util.Optional.of(new ByteArrayResource(new byte[100])));
+//
+//
+//        mockMvc.perform(get("/map/storage/{uuid}", UUID.randomUUID())
+//                ).andExpect(status().isOk())
+//                 .andDo(
+//                         document("get-done-image",
+//                                 pathParameters(
+//                                         parameterWithName("uuid").description("이미지 UUID")
+//                                 )
+//                         )
+//                 );
+//    }
 }
