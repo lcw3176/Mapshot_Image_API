79ee6778b120ddf0d39c2e0aea7489b4f6817250
Chanwoo Lee <lcw3176@naver.com>
2022-07-06 21:11:33 +0900

    Hotfix/enum 이름 변경 (#28)
    * 이름 변경
    
    * 소문자로 변경

--- src/test/java/com/joebrooks/mapshotimageapi/map/google/GoogleMapGeneratorControllerTest.java
+++ src/test/java/com/joebrooks/mapshotimageapi/map/google/GoogleMapGeneratorControllerTest.java
@@ -38,7 +38,7 @@ class GoogleMapGeneratorControllerTest {
         UserMapRequest userMapRequest = UserMapRequest.builder()
                 .lat(37.566968566622464)
                 .lng(127.05034726055617)
-                .companyType(CompanyType.GOOGLE)
+                .companyType(CompanyType.google)
                 .layerMode(false)
                 .level(2)
                 .type("basic")
