de22e562853a73b75feb88d32640c84fc01a1c92
lcw3176 <lcw3176@naver.com>
2022-08-07 14:01:50 +0900

    redis 제거

--- src/main/java/com/joebrooks/mapshotimageapi/storage/StorageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.joebrooks.mapshotimageapi.storage;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StorageRepository extends CrudRepository<Storage, String> {
-    Storage findByUuid(String uuid);
-}
