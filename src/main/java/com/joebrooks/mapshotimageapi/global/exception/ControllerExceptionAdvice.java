2ccee5efe269a08186d366be12b7a107482e7b7d
lcw3176 <lcw3176@naver.com>
2022-11-07 13:51:23 +0900

    messageExceptionHandler 추가

--- src/main/java/com/joebrooks/mapshotimageapi/global/exception/ControllerExceptionAdvice.java
+++ src/main/java/com/joebrooks/mapshotimageapi/global/exception/ControllerExceptionAdvice.java
@@ -3,6 +3,7 @@ package com.joebrooks.mapshotimageapi.global.exception;
 import com.joebrooks.mapshotimageapi.global.sns.SlackClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,6 +16,12 @@ public class ControllerExceptionAdvice {
 
     @ExceptionHandler(Exception.class)
     public void exceptionHandler(Exception exception){
+        log.error(exception.getMessage(), exception);
+        slackClient.sendMessage(exception);
+    }
+
+    @MessageExceptionHandler(Exception.class)
+    public void messageExceptionHandler(Exception exception){
         log.error(exception.getMessage(), exception);
         slackClient.sendMessage(exception);
     }
